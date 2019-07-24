779c9759acc2ca6e2f27e70b4a5434c3e39ee8a6
Edmund O. Mati Jr <ejvaux_05126@yahoo.com>
2019-07-24 16:07:15 +0800

    modified scan page design. removed loading of tables when scanning.

--- resources/views/includes/scan/sntab.blade.php
+++ resources/views/includes/scan/sntab.blade.php
@@ -1,12 +1,12 @@
-<div class="row mt-2 mb-1">        
+<div class="row mt-2 mb-2">        
     <div class="col-md-3">
-        <button id='loadpcbtable' class='btn btn-primary'><i class="fas fa-sync-alt"></i> LOAD</button>
+        <button id='loadpcbtable' class='btn btn-primary'><i class="fas fa-sync-alt"></i> REFRESH</button>
     </div>
     {{-- <div class="col-md-5">
         <input class='form-control' type="text" name="" id="searchpcbtable" placeholder="Scan Serial Number Here to Search">
     </div> --}}
     <div class="col-md">
-        <span class='text-danger bold-text'>Serial numbers are based on the Configuration and selected Work Order.</span>
+        <span class='text-danger bold-text'>Serial numbers are based on the Configuration and selected Work Order.<br>Click [REFRESH] button to refresh data.</span>
     </div>
 </div>
 <div class="row">
