aca6652b07ced90aab5189757d132b7767fa17aa
Edmund O. Mati Jr <ejvaux_05126@yahoo.com>
2019-11-14 08:31:04 +0800

    Add accuracy column - material_reports

---
+++ database/migrations/2019_11_14_082703_add_accuracy_column_material_reports_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAccuracyColumnMaterialReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('material_reports', function (Blueprint $table) {
+            $table->string('accuracy')->nullable()->after('sys_qty');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('material_reports', function (Blueprint $table) {
+            $table->dropColumn([
+                'accuracy',
+                ]);
+        });
+    }
+}
